050a1ed68a5e17039416f5419fa19bae245a6fb9
Jeremy Brenner <jeremyjbrenner@gmail.com>
2013-03-18 22:20:37 -0500

    moved mockup out of the layout and into a view, added view for campaigns mockup, added routes for both

--- app/controllers/crank_test_controller.rb
+++ app/controllers/crank_test_controller.rb
@@ -1,5 +1,11 @@
 class CrankTestController < ApplicationController
   def home
     #render :layout => false
+  end  
+  def campaigns_mockup
+    #render :layout => false
+  end  
+  def campaign_details_mockup
+    #render :layout => false
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 CrankTest::Application.routes.draw do 
 
   match '/', to: 'crank_test#home'
+  
+  match '/campaigns_mockup', to: 'crank_test#campaigns_mockup'
+  
+  match '/campaign_details_mockup', to: 'crank_test#campaign_details_mockup'
 
   resources :days
 
